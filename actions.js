1324a4e2aa7f39b685e0c373347fbff28330ae2a
Christina Hopperstad <admin@merle.no>
2023-11-06 10:45:57 +0100

    Endringer

--- actions.js
+++ actions.js
@@ -1,3 +1,20 @@
-/* const footerContainer = document.getElementById("footer-container");
-const footerContent = document.getElementById("footer-content")
-footerContainer.appendChild(footerContent) */
+const button = document.getElementById("email-button");
+const modalContainer = document.getElementById("modal-container");
+console.log(modalContainer);
+
+button.addEventListener("click", () => {
+  /* const response = await fetch("some-url") */
+  modalContainer.removeChild(document.getElementById("footer-newsletter"))
+  const section = document.createElement("section")
+  section.classList.add("modal-container")
+  /* const div = document.createElement("div")
+  div.classList.add("inner-modal-container") */
+
+  const p = document.createElement("p")
+  p.textContent = "Takk for din påmelding!"
+  p.classList.add("modal-text")
+  modalContainer
+    .appendChild(section)
+    /* .appendChild(div) */
+    .appendChild(p)
+});
